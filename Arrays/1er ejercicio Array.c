bd44db968c8f60109528371a1062e9c4f82c4aad
Mily <mroble@etrr.edu.ar>
2025-10-01 10:46:36 -0300

    1er ejercicio ; Suma de vectores

---
+++ Arrays/1er ejercicio Array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sumarVector(int v[], int n) {
+    int suma = 0;
+    for(int i = 0; i < n; i++) {
+        suma += v[i];
+    }
+    return suma;
+}
+int main() {
+    int tama;
+    printf("Ingrese el tamaño del vector: ");
+    scanf("%d", &tama);
+    
+    int numeros[tama];
+    for(int i = 0; i < tama; i++) {
+        printf("Ingrese el elemento %d: ", i + 1);
+        scanf("%d", &numeros[i]);
+    }
+    int resultado = sumarVector(numeros, tama);
+    printf("La suma de los elementos es: %d\n", resultado);
+    return 0;
+}
