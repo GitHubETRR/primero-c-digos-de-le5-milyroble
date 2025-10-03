e9358ce87f25248992f63435824993a62f635d2c
Mily <mroble@etrr.edu.ar>
2025-10-03 12:40:59 -0300

    Se ingresan elementos, un vector los suma y se muestran el resultado y final y promedio

--- Arrays/1er y 2do ejercicio Array.c
+++ Arrays/1er y 2do ejercicio Array.c
@@ -2,22 +2,25 @@
 
 int sumarVector(int v[], int n) {
     int suma = 0;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         suma += v[i];
     }
     return suma;
 }
+
 int main() {
     int tama;
+    int numeros[tama];
+    int resultado = sumarVector(numeros, tama);
+    int prom = resultado / tama;
     printf("Ingrese el tamaño del vector: ");
     scanf("%d", &tama);
-    
-    int numeros[tama];
-    for(int i = 0; i < tama; i++) {
+    for (int i = 0; i < tama; i++) {
         printf("Ingrese el elemento %d: ", i + 1);
         scanf("%d", &numeros[i]);
     }
-    int resultado = sumarVector(numeros, tama);
     printf("La suma de los elementos es: %d\n", resultado);
+    printf("El promedio de los elementos ingresados es: %d\n", prom);
+
     return 0;
-} 
+}
