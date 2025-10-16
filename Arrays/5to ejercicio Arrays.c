45dc8fe88f112d72ae4ca7ecd2be006888241439
Mily <mroble@etrr.edu.ar>
2025-10-16 16:09:09 -0300

    Add files via upload

---
+++ Arrays/5to ejercicio Arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, A[10], B[10], prod = 0;
+    printf("Tamaño de los vectores: ");
+    scanf("%d", &n);
+    printf("Elementos de A:\n");
+    for(i = 0; i < n; i++) scanf("%d", &A[i]);
+    printf("Elementos de B:\n");
+    for(i = 0; i < n; i++) scanf("%d", &B[i]);
+    for(i = 0; i < n; i++) prod += A[i] * B[i];
+    printf("Producto escalar = %d", prod);
+    return 0;
+}
