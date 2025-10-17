bf2ea7f63c2941b4c49618340a85d1b77d0f4024
Mily <mroble@etrr.edu.ar>
2025-10-17 12:05:42 -0300

    Update 5to ejercicio Arrays.c : For´s corregidos + If para verificar que el tamaño no sea negativo

--- Arrays/5to ejercicio Arrays.c
+++ Arrays/5to ejercicio Arrays.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int n, i; A[n], B[n];
+    int n, i;
     int prod = 0;
+
     printf("Tamaño de los vectores: ");
     scanf("%d", &n);
+
+    if(n <= 0) {
+        printf("El tamaño debe ser mayor a 0.\n");
+        return 0;
+    }
+    int vectorA[n], vectorB[n]; 
+    
     printf("Elementos de A:\n");
-    for(i = 0; i < n; i++) scanf("%d", &A[i]);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &vectorA[i]);
+    }
     printf("Elementos de B:\n");
-    for(i = 0; i < n; i++) scanf("%d", &B[i]);
-    for(i = 0; i < n; i++) prod += A[i] * B[i];
-    printf("Producto escalar = %d", prod);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &vectorB[i]);
+    }
+    for(i = 0; i < n; i++) {
+        prod += vectorA[i] * vectorB[i];
+    }
+    printf("Resultado del producto escalar = %d\n", prod);
     return 0;
 }
