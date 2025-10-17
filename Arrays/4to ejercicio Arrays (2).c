bf7e8b91a125da4e5542c971e797e0b6bb789895
Mily <mroble@etrr.edu.ar>
2025-10-17 11:50:03 -0300

    Update 4to ejercicio Arrays.c : For´s corregidos + Do while para rango de números ingresados

--- Arrays/4to ejercicio Arrays (2).c
+++ Arrays/4to ejercicio Arrays (2).c
@@ -1,29 +1,40 @@
 #include <stdio.h>
-#define TAM 100
+#define tamanio 100
 
 int main() {
-    int v[TAM];
+    int v[tamanio];
     int i, max, min;
 
     printf("Elegí 100 valores entre 10 y 40:\n");
-    for(i = 0; i < TAM; i++) {
-        scanf("%d", &v[i]);
+
+    for(i = 0; i < tamanio; i++) {
+        do {
+            scanf("%d", &v[i]);
+
+            if(v[i] < 10 || v[i] > 40)
+                printf("Valor fuera de rango. Ingresá un número entre 10 y 40.\n");
+
+        } while(v[i] < 10 || v[i] > 40);
     }
 
     max = v[0];
     min = v[0];
-    for(i = 1; i < TAM; i++) {
-        if(v[i] > max) max = v[i];
-        if(v[i] < min) min = v[i];
+
+    for(i = 1; i < tamanio; i++) {
+        if(v[i] > max)
+            max = v[i];
+        if(v[i] < min)
+            min = v[i];
     }
 
     printf("El valor máximo es %d\n", max);
     printf("El valor mínimo es %d\n", min);
-    printf("El máximo se repite en posiciones: ");
-    for(i = 0; i < TAM; i++)
+    printf("El máximo se repite en la posición: ");
+
+    for(i = 0; i < tamanio; i++)
         if(v[i] == max)
             printf("%d ", i);
-    printf("\n");
 
+    printf("\n");
     return 0;
 }
